29814c357e523dd426e5d6937c28228f7cc8d000
Bill Dengler <codeofdusk@gmail.com>
2017-06-16 21:25:01 +0000

    Initial (automated) port to Python3.

--- src/extra/AudioUploader/transfer.py
+++ src/extra/AudioUploader/transfer.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from __future__ import division
+from builtins import object
+from past.utils import old_div
 import sys
 import threading
 import time
