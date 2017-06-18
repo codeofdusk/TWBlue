55c18b09c8484872d73ffb2e9700bc6a345adede
Manuel Cortéz <manuel@manuelcortez.net>
2017-06-17 21:39:39 -0500

    Fixed sound.py when there are multiple sessions trying to load the library

--- src/sound.py
+++ src/sound.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from builtins import range
-from builtins import object
 import sys
 import url_shortener
 import audio_services
@@ -23,6 +21,8 @@ import application
 import tempfile
 import glob
 URLPlayer = None
+
+is_created = False
 
 def setup():
  global URLPlayer
@@ -60,23 +60,26 @@ class soundSystem(object):
 
  def __init__(self, soundConfig):
   """ Sound Player."""
+  global is_created
   self.config = soundConfig
   # Set the output and input default devices.
-  try:
-   self.output = Output()
-   self.input = Input()
-  except IOError:
-   pass
+  if is_created == False:
+   try:
+    self.output = Output()
+    self.input = Input()
+   except IOError:
+    pass
   # Try to use the selected device from the configuration. It can fail if the machine does not has a mic.
-  try:
-   log.debug("Setting input and output devices...")
-   self.output.set_device(self.output.find_device_by_name(self.config["output_device"]))
-   self.input.set_device(self.input.find_device_by_name(self.config["input_device"]))
-  except:
-   log.exception("Error in input or output devices, using defaults...")
-   self.config["output_device"] = "Default"
-   self.config["input_device"] = "Default"
-
+  if is_created == False:
+   try:
+    log.debug("Setting input and output devices...")
+    self.output.set_device(self.output.find_device_by_name(self.config["output_device"]))
+    self.input.set_device(self.input.find_device_by_name(self.config["input_device"]))
+   except:
+    log.exception("Error in input or output devices, using defaults...")
+    self.config["output_device"] = "Default"
+    self.config["input_device"] = "Default"
+  is_created = True
   self.files = []
   self.cleaner = RepeatingTimer(60, self.clear_list)
   self.cleaner.start()
@@ -146,7 +149,7 @@ class URLStream(object):
   elif stream != None:
    self.stream=stream
   if self.prepared == True:
-   self.stream = SoundlibURLStream(url=bytes(self.url,'utf-8'))
+   self.stream = SoundlibURLStream(url=self.url)
   if hasattr(self,'stream'):
    self.stream.volume = float(volume)
    self.stream.play()
